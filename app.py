3ac3f7ec3da7aec346ef800bdf0dcd14bc931bac
Discover Devops <53135263+discover-devops@users.noreply.github.com>
2025-10-16 08:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is my Jenkins CI/CD pipeline demo."
+    return "Hello, we are done with jenkins, going to start terraform."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
